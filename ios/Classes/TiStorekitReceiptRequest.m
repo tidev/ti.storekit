7bd6f50e4815249f590810b24bc871b148ac19d0
jonalter <jonalter+github@gmail.com>
2013-09-25 13:14:44 -0700

    [MOD-1557] Updating license info

--- ios/Classes/TiStorekitReceiptRequest.m
+++ ios/Classes/TiStorekitReceiptRequest.m
@@ -1,6 +1,7 @@
 /**
- * Ti.Storekit Module
+ * Appcelerator Titanium Mobile
  * Copyright (c) 2010-2013 by Appcelerator, Inc. All Rights Reserved.
+ * Licensed under the terms of the Apache Public License
  * Please see the LICENSE included with this distribution for details.
  */
 
